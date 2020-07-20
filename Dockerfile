345897d777c5d92bceaa94a30e0a0c26cdbcb27c
jckballero <jean.caballero29@hotmail.com>
2020-07-20 06:47:54 +0000

    cambios finales agregados

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:latest
 
 WORKDIR /src
 
-COPY package.json ./
+COPY package*.json ./
 RUN npm install
 
 COPY . .
--- laboratorio6.js
+++ laboratorio6.js
@@ -1,7 +1,48 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser');
 const port = 5000
- 
-app.get('/', (req, res) => res.send('Prueba docker'))
- 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+const ESTUDIANTES = [
+    {
+        nombre: "Jean Caballero",
+        edad: 29,
+    },
+    {
+        nombre: "Carlos Sánchez",
+        edad: 30,
+    }
+
+];
+
+app.use(bodyparser.json());
+
+//CONTROLADORES
+app.get('/api/estudiantes/', (req, res) =>{
+    res.json({
+        cantidad: ESTUDIANTES.length,
+        estudiantes: ESTUDIANTES
+    });
+});
+
+app.get('/api/estudiantes/:id',(req, res) => {
+    res.json(ESTUDIANTES[req.params.id]);
+});
+
+app.post('/api/estudiantes/',(req, res) => {
+   ESTUDIANTES.push(req.body);
+   res.json(req.body);
+});
+
+app.put('/api/estudiantes/:id',(req, res) => {
+    ESTUDIANTES.push(req.body);
+    res.json(Estudiantes[req.params.id]);
+});
+  
+app.delete('/api/estudiantes/:id',(req, res) => {
+    res.json(Estudiantes.splice(req.params.id));
+    res.send(Estudiantes)
+});
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
